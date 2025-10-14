18a4517464886833bb2515f4c9556f686de9259d
Ziaul Khan <ziaul@teamofkeys.com>
2025-10-14 18:31:31 +0530

    Enhanced InfoSection component by adding new info cards with icons, updated InfoSectionCard to support image props, and implemented a View More button for expanded card visibility.

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,73 +1,166 @@
 import React, { useState } from "react";
 import InfoSectionCard from "./InfoSectionCard";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { RiAuctionFill } from "react-icons/ri";
+import { FaPhotoFilm } from "react-icons/fa6";
+import {
+  MdAppRegistration,
+  MdOutlineCarRental,
+  MdOutlineCarRepair,
+  MdOutlineManageHistory,
+  MdOutlinePerson,
+  MdOutlinePriceChange,
+  MdOutlineSyncProblem,
+  MdYoutubeSearchedFor,
+} from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
+import { PiNumberSevenFill } from "react-icons/pi";
+import { BiSolidSpreadsheet } from "react-icons/bi";
+import { AiFillDatabase } from "react-icons/ai";
 
 const infoCards = [
   {
     id: 1,
     title: "Recall​",
-    icon: "/recall.svg",
+    img: "/recall.svg",
   },
   {
     id: 2,
     title: "*KM History​",
-    icon: "/meter.svg",
+    img: "/meter.svg",
   },
 
   {
     id: 3,
     title: "Acceptance in Insurance​",
-    icon: "/secure.svg",
+    img: "/secure.svg",
   },
   {
     id: 4,
     title: "Car Loss Rate​",
-    icon: "/carCrash.svg",
+    img: "/carCrash.svg",
   },
   {
     id: 5,
     title: "Gravame",
-    icon: "/recall.svg",
+    img: "/recall.svg",
   },
   {
     id: 6,
     title: "Robbery & Theft​",
-    icon: "/carThief.svg",
+    img: "/carThief.svg",
   },
   {
     id: 7,
     title: "Approximate Maintenance​ cost",
-    icon: "/maintenance.svg",
+    img: "/maintenance.svg",
   },
   {
     id: 8,
     title: "Lighthouse​",
-    icon: "/lighthouse.svg",
+    img: "/lighthouse.svg",
   },
   {
     id: 9,
     title: "Debts & Fines​",
-    icon: "/debt.svg",
+    img: "/debt.svg",
   },
   {
     id: 10,
     title: "Restrictions & impediments​",
-    icon: "/meter.svg",
+    img: "/meter.svg",
   },
   {
     id: 11,
     title: "Beats​",
-    icon: "/secure.svg",
+    img: "/secure.svg",
   },
   {
     id: 12,
-    title: "Fipe​",
-    icon: "/carCrash.svg",
+    title: "FIPE​",
+    img: "/carCrash.svg",
+  },
+  {
+    id: 13,
+    title: "Auction Data",
+    icon: <RiAuctionFill className="text-[#1AABFE]" />,
+  },
+  {
+    id: 14,
+    title: "Vehicle Photos",
+    icon: <FaPhotoFilm className="text-[#1AABFE]" />,
+  },
+  {
+    id: 15,
+    title: "Search History",
+    icon: <MdYoutubeSearchedFor className="text-[#1AABFE]" />,
+  },
+  {
+    id: 16,
+    title: "Maintenance History",
+    icon: <MdOutlineManageHistory className="text-[#1AABFE]" />,
+  },
+  {
+    id: 17,
+    title: "Chassis Explanation",
+    icon: <MdOutlineCarRepair className="text-[#1AABFE]" />,
+  },
+  {
+    id: 18,
+    title: "Main model problems",
+    icon: <MdOutlineSyncProblem className="text-[#1AABFE]" />,
+  },
+  {
+    id: 19,
+    title: "Ownership",
+    icon: <MdOutlineCarRental className="text-[#1AABFE]" />,
+  },
+  {
+    id: 20,
+    title: "Price History",
+    icon: <FaHistory className="text-[#1AABFE]" />,
+  },
+  {
+    id: 21,
+    title: "Registration data state",
+    icon: <MdAppRegistration className="text-[#1AABFE]" />,
+  },
+  {
+    id: 22,
+    title: "Vehicle Numbers",
+    icon: <PiNumberSevenFill className="text-[#1AABFE]" />,
+  },
+  {
+    id: 23,
+    title: "Registration in rental companies",
+    icon: <MdOutlineCarRental className="text-[#1AABFE]" />,
+  },
+  {
+    id: 24,
+    title: "Technical sheet",
+    icon: <BiSolidSpreadsheet className="text-[#1AABFE]" />,
+  },
+  {
+    id: 25,
+    title: "Fair Price",
+    icon: <MdOutlinePriceChange className="text-[#1AABFE]" />,
+  },
+  {
+    id: 26,
+    title: "Owner's Opinion",
+    icon: <MdOutlinePerson className="text-[#1AABFE]" />,
+  },
+  {
+    id: 27,
+    title: "Vehicle Registration Data",
+    icon: <AiFillDatabase className="text-[#1AABFE]" />,
   },
 ];
 
 const InfoSection = () => {
   const [selectedCard, setSelectedCard] = useState(null);
+  const [showMore, setShowMore] = useState(false);
+
   const handleCardClick = (card) => {
     setSelectedCard(card);
   };
@@ -94,20 +187,29 @@ const InfoSection = () => {
         </div>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full mt-6 lg:mt-12 gap-4">
-        {infoCards.map((card) => (
+        {infoCards.slice(0, showMore ? infoCards.length : 12).map((card) => (
           <InfoSectionCard
             key={card.id}
             title={card.title}
+            img={card.img}
             icon={card.icon}
             onClick={() => handleCardClick(card.id)}
-            isSelected={selectedCard === card.id}
           />
         ))}
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center gap-4 md:gap-8 my-10 mb-4 md:mb-6">
-        <button className="bg-[#1AABFE] rounded-md flex items-center gap-3 py-3 px-8 w-fit cursor-pointer hover:bg-[#1AABFE]/80 transition-all duration-300 text-white">
-          <span className="md:text-[1.4rem] font-medium">View More</span>
-          <FaArrowRightLong className=" md:text-[1.3rem] transition-all duration-300 -rotate-45" />
+        <button
+          className="bg-[#1AABFE] rounded-md flex items-center gap-3 py-3 px-8 w-fit cursor-pointer hover:bg-[#1AABFE]/80 transition-all duration-300 text-white"
+          onClick={() => setShowMore(!showMore)}
+        >
+          <span className="md:text-[1.4rem] font-medium">
+            {showMore ? "View Less" : "View More"}
+          </span>
+          <FaArrowRightLong
+            className={` md:text-[1.3rem] transition-all duration-300 ${
+              showMore ? "rotate-0" : "-rotate-45"
+            }`}
+          />
         </button>
       </div>
     </div>
--- src/components/InfoSectionCard.jsx
+++ src/components/InfoSectionCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const InfoSectionCard = ({ title, icon, isSelected, onClick }) => {
+const InfoSectionCard = ({ title, img, icon, isSelected, onClick }) => {
   return (
     <div
       className={`flex flex-col items-center justify-center gap-5 rounded-2xl border-2 border-gray-400/20 w-full px-4 py-5 shadow cursor-pointer hover:scale-105 transition-all duration-300 ${
@@ -8,7 +8,13 @@ const InfoSectionCard = ({ title, icon, isSelected, onClick }) => {
       } transition-all duration-300 text-center`}
       onClick={onClick}
     >
-      <img src={icon} alt="" className="size-8 md:size-10" />
+      {icon ? (
+        <div className="[&>*]:!text-black text-[1.5rem] md:text-[2rem]">
+          {icon}
+        </div>
+      ) : (
+        <img src={img} alt="" className="size-8 md:size-10" />
+      )}
       <p className={`font-semibold ${isSelected ? "text-white" : ""}`}>
         {title}
       </p>
